cd8d1f5d5cb6607fc6a5796a4dfa55603db25eb2
sharabha <sharabhareddyk@gmail.com>
2025-04-28 08:00:12 +0530

    predefined

---
+++ eclipse-workspace/JAVA-FSD/Predefined/PredefinedClasses.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class PredefinedClasses {
+  public static void main(String[] args) {
+	 ArrayList<Integer> li=new ArrayList<>();
+	 System.out.println(li.size());
+  }
+}
