68347467e5245c2460af52155301a1c7cd6b2772
sharabha <sharabhareddyk@gmail.com>
2025-05-01 12:16:43 +0530

    kiolmm

--- eclipse-workspace/JAVA-FSD/Encapsulation/Main8.java
+++ eclipse-workspace/JAVA-FSD/Encapsulation/Main8.java
@@ -1,11 +1,18 @@
 
 public class Main8 {
+	static int a=10;
+	int b=20;
+	private int c=30;
 	public static void main(String[] args) {
+		Ecapsulation ef =new Ecapsulation("kakanuriSharabha","sharabhareddyk@gamil.com");
+		System.out.println(ef.getName());
+		ef.setName("Reddy");
+		System.out.println(ef.getName());
 		
-		Ecapsulation e =new Ecapsulation("kakanuri Sharabha","sharabhareddyk@gamil.com");
-		System.out.println(e.getName());
-		e.setName("Reddy");
-		System.out.println(e.getName());
+		Main8 h=new Main8();
+		System.out.println(a);
+		System.out.println(h.b);
+		System.out.println(h.c);
 	}
 
 }
--- eclipse-workspace/JAVA-FSD/Polymorphisim/Main9.java
+++ eclipse-workspace/JAVA-FSD/Polymorphisim/Main9.java
@@ -1,6 +1,34 @@
 
 public class Main9 {
+	
+	public static  class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+	public  class Product1{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 	public static void main(String[] args) {
+	    Product pp=new Product();
+	    System.out.println(pp.product(6, 9));
+	    Main9 mm=new Main9();
+	    Main9.Product1 p1=mm.new Product1();
+	    System.out.println(p1.product(9, 8));
 		Compiletimepolymorphism cp=new Compiletimepolymorphism ();
 		cp.add(4, 7);
 		cp.add(3, 4,5);
@@ -8,3 +36,4 @@ public class Main9 {
 			
 		}
 }
+
