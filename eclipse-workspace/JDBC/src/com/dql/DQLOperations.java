baa6b3077ae7b615923b4c5b4010c2b8af79ac8c
sharabha <sharabhareddyk@gmail.com>
2025-07-16 18:18:18 +0530

    update1

--- eclipse-workspace/JDBC/src/com/dql/DQLOperations.java
+++ eclipse-workspace/JDBC/src/com/dql/DQLOperations.java
@@ -1,5 +1,32 @@
 package com.dql;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.util.JdbcUtil;
+
 public class DQLOperations {
+	public static void main(String[] args)  {
+		
+		try {
+			JdbcUtil jdbcutil=new JdbcUtil();
+			Connection connection=jdbcutil.getConnection();
+			Statement statement=connection.createStatement();
+			String query="Select * from questions";
+			ResultSet resultset=statement.executeQuery(query);
+			while (resultset.next()) {
+				int id=resultset.getInt(1);
+				String name=resultset.getString(2);
+				System.out.println(id+"\t"+name);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
--- eclipse-workspace/JDBC/src/com/util/JdbcUtil.java
+++ eclipse-workspace/JDBC/src/com/util/JdbcUtil.java
@@ -1,5 +1,16 @@
 package com.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcUtil {
+	public Connection connection=null;
+	public Connection getConnection() throws SQLException {
+		if(connection==null) {
+			 connection=DriverManager.getConnection(Constants.url,Constants.username,Constants.password);
+		}
+		return connection;
+	}
 
 }
