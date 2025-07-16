2b42ba58e35fb9491ad7aa465699a28cce21fb0b
sharabha <sharabhareddyk@gmail.com>
2025-07-16 13:25:50 +0530

    update

---
+++ eclipse-workspace/JDBC/src/com/dml/DMLOperations.java
@@ -0,0 +1,5 @@
+package com.dml;
+
+public class DMLOperations {
+
+}
---
+++ eclipse-workspace/JDBC/src/com/util/Constants.java
@@ -0,0 +1,8 @@
+package com.util;
+
+public class Constants {
+	public static final String url="jdbc:mysql://localhost:3306/questionsdb";
+	public static final String username="root";
+	public static final String password="Adilakshmi@1";
+
+}
